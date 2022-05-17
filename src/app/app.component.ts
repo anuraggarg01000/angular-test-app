2c6f33cc45a0d527a4065d1ac022110d6fc36536
Anurag <anurag.garg@affordplan.com>
2022-05-17 17:18:17 +0530

    test-app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'test-app';
+  title = 'Test-app'
+  success_msg = true
+  superPower ='wonderwoman'
+  styleProp = 'purple'
+  contacts =[
+    {
+      'FirstName' : 'Anurag',
+      'LastName'  : 'Garg',
+      'Age'        : "20",
+    }, 
+    {
+      'FirstName' : 'Manav',
+      'LastName'  : 'Bansal',
+      'Age'        : "21",
+    }
+  ]
 }
